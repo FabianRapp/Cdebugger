eb5a9b053a5083fe8200412cc7e55070bf2470fc
FabianRapp <fa.rapp@web.de>
2024-10-28 20:24:29 +0100

    small refactor

--- srcs/debug/classes/MemMaps.cpp
+++ srcs/debug/classes/MemMaps.cpp
@@ -1,5 +1,73 @@
 #include <MemMaps.hpp>
 
+static void	parse_range(t_range &cur, std::string &entry) {
+	std::stringstream	start;
+	start << std::hex << entry.substr(0, entry.find('-'));
+	start >> cur.start;
+	std::stringstream	end;
+	end << std::hex << entry.substr(entry.find('-') + 1, -1);
+	end >> cur.end;
+}
+
+static void	parse_permissions(t_range &cur, std::string &entry) {
+	if (entry[0] == '-') {
+		cur.read = false;
+	} else {
+		assert(entry[0] == 'r');
+		cur.read = true;
+	}
+	if (entry[1] == '-') {
+		cur.write = false;
+	} else {
+		assert(entry[1] == 'w');
+		cur.write = true;
+	}
+	if (entry[2] == '-') {
+		cur.execute = false;
+	} else {
+		assert(entry[2] == 'x');
+		cur.execute = true;
+	}
+	if (entry[3] == 'p') {
+		cur.shared = false;
+	} else {
+		assert(entry[4] == 's');
+		cur.shared = true;
+	}
+}
+
+static void	parse_offset(t_range &cur, std::string &entry) {
+	std::stringstream	offset;
+	offset << std::hex << entry;
+	offset >> cur.offset;
+}
+
+static void	parse_device(t_range &cur, std::string &entry) {
+	std::stringstream	device_minor;
+	device_minor << std::hex << entry.substr(0, entry.find(':'));
+	device_minor >> cur.device_minor;
+	std::stringstream	device_major;
+	device_major << std::hex << entry.substr(entry.find(':') + 1, -1);
+	device_major >> cur.device_major;
+}
+
+static void	parse_inode(t_range &cur, std::string &entry) {
+	std::stringstream	inode;
+	inode << entry;
+	inode >> cur.inode;
+}
+
+static void	parse_path(t_range &cur, std::string &entry, std::ifstream &file,
+	std::streampos &old_pos) {
+	// if no path entry
+	if (entry[0] != '/' && entry[0] != '[') {
+		cur.path = "";
+		file.seekg(old_pos);
+	} else {
+		cur.path = entry;
+	}
+}
+
 MemMaps::MemMaps(pid_t pid) {
 	std::string		path = "/proc/" + std::to_string(pid) + "/maps";
 	std::ifstream	file(path);
@@ -14,86 +82,37 @@ MemMaps::MemMaps(pid_t pid) {
 	
 		for (int i = 0; i < 6; i++) {
 			std::string	entry;
-
-
 			std::streampos	old_pos = file.tellg();
 			if (!(file >> entry)) {
 				break ;
 			}
-
+			// todo: wtf is this
 			if (entry == "(deleted)") {
 				i--;
 				continue ;
 			}
-			std::cout << i << ": " << entry << std::endl;
 			switch (i) {
 				case(0): {
-					std::stringstream	start;
-					start << std::hex << entry.substr(0, entry.find('-'));
-					start >> cur.start;
-					std::stringstream	end;
-					end << std::hex << entry.substr(entry.find('-') + 1, -1);
-					end >> cur.end;
+					parse_range(cur, entry);
 					break ;
-				}
-				case (1): {
-					if (entry[0] == '-') {
-						cur.read = false;
-					} else {
-						assert(entry[0] == 'r');
-						cur.read = true;
-					}
-					if (entry[1] == '-') {
-						cur.write = false;
-					} else {
-						assert(entry[1] == 'w');
-						cur.write = true;
-					}
-					if (entry[2] == '-') {
-						cur.execute = false;
-					} else {
-						assert(entry[2] == 'x');
-						cur.execute = true;
-					}
-					if (entry[3] == 'p') {
-						cur.shared = false;
-					} else {
-						assert(entry[4] == 's');
-						cur.shared = true;
-					}
+				} case (1): {
+					parse_permissions(cur, entry);
 					break ;
-				}
-				case (2): {
-					std::stringstream	offset;
-					offset << std::hex << entry;
-					offset >> cur.offset;
+				} case (2): {
+					parse_offset(cur, entry);
 					break ;
-				}
-				case (3): {
-					std::stringstream	device_minor;
-					device_minor << std::hex << entry.substr(0, entry.find(':'));
-					device_minor >> cur.device_minor;
-					std::stringstream	device_major;
-					device_major << std::hex << entry.substr(entry.find(':') + 1, -1);
-					device_major >> cur.device_major;
+				} case (3): {
+					parse_device(cur, entry);
 					break ;
-				}
-				case (4): {
-					std::stringstream	inode;
-					inode << entry;
-					inode >> cur.inode;
+				} case (4): {
+					parse_inode(cur, entry);
 					break ;
-				}
-				case (5): {
-					if (entry[0] != '/' && entry[0] != '[') {
-						cur.path = "";
-						file.seekg(old_pos);
-						break ;
-					}
-					cur.path = entry;
+				} case (5): {
+					parse_path(cur, entry, file, old_pos);
 					break ;
+				} default: {
+					assert(0);
 				}
-				default: assert(0);
 			}
 		}
 		this->ranges.push_back(cur);
